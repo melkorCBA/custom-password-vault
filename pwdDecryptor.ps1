23c0f03d4de5f09d66c4511906b5b414b135a1f4
Bashika De Silva <BashikaDS@compello.com>
2021-02-15 01:47:31 +0530

    init commit

---
+++ pwdDecryptor.ps1
@@ -0,0 +1,54 @@
+﻿param (
+        $pwdSearchKey
+    )
+
+Clear-Host
+
+#authenticate local user 
+$cred = Get-Credential #Read credentials
+ $username = $cred.username
+ $password = $cred.GetNetworkCredential().password
+
+$computer = $env:COMPUTERNAME
+
+Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+$obj = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine',
+$computer)
+$isAuthenticated =  $obj.ValidateCredentials($username, $password) 
+
+if($isAuthenticated){
+    write-host "Successfully authenticated"
+
+    $documentPath = [Environment]::GetFolderPath("MyDocuments")
+    $vault = $documentPath + "\vault\"
+    $files = get-childitem $vault |  where { $_.name -match "^pwd[a-zA-Z0-9]*.txt"} |  select -ExpandProperty name
+
+
+    $regex = '^pwd(' +$pwdSearchKey + ')*.txt'
+    ForEach($file in $files){
+        if($file -match $regex){
+            $pwdFile = $file
+        }
+        
+    }
+
+    
+    if(-not ([string]::IsNullOrEmpty($pwdFile))){
+        $pwdFilePath = $vault + $pwdFile
+        $securestring = convertto-securestring -string (get-content $pwdFilePath)
+        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securestring)
+        $passwsord = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+        Clear-Host
+        write-host $passwsord 
+    }
+    else {
+        write-host "no such key in the vault" 
+    }
+}
+else{
+    write-host "Authentication failed - please verify your username and password."
+    exit #terminate the script.
+}
+
+
+
---
+++ pwdEncriptor.ps1
@@ -0,0 +1,24 @@
+﻿param (
+        $storeKey
+    )
+
+$documentPath = [Environment]::GetFolderPath("MyDocuments")
+$vault = $documentPath + "\vault\"
+$fliePath = $vault + "pwd" + $storeKey + ".txt"
+$pass = Read-Host 'password?' -AsSecureString
+
+$password = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass))
+
+# Create vault if does not exist
+if (!([System.IO.Directory]::Exists($vault))){
+[system.io.directory]::CreateDirectory($vault)
+}
+
+    
+# Create file if does not exist
+if (!(Test-Path $fliePath)){
+    New-Item -path $fliePath
+}
+
+convertto-securestring -string $password -asplaintext -force | convertfrom-securestring | out-file $fliePath
